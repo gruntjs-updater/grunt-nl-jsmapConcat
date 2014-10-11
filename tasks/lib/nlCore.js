c89a53ca650ef21bfe69426b6be4f9f6c4e4e730
tech-gzq <1352139287@qq.com>
2014-10-11 09:30:05 +0800

    update

--- tasks/lib/nlCore.js
+++ tasks/lib/nlCore.js
@@ -1,10 +1,11 @@
 /**
  * Created by gzq on 14-2-25.
  */
-exports.core = function () {
+exports.core = function (grunt,comment,banner,footer,options) {
     var $NL = {};
     $NL.script={};
     $NL.script.jsmaps=[];
+    $NL.f=null;
     /**
      * 判断一个变量是否是Function类型
      * @param {Object} fn 要判断的变量
@@ -46,7 +47,46 @@ exports.core = function () {
     }
 
     $NL.script.pushMap=function(arr){
-        $NL.script.jsmaps=$NL.script.jsmaps.concat(arr);
+        //$NL.script.jsmaps=$NL.script.jsmaps.concat(arr);
+        $NL.script.jsmaps=arr;
+        if(arr[0].key=="admin_jsmapModule"){
+            grunt.log.writeln(arr[0].compress);
+        }
+        if (!!$NL.script.jsmaps && $NL.script.jsmaps.length > 0) {
+            //grunt.log.writeln("length:" + $NL.script.jsmaps.length);
+            for (var i = 0; i < $NL.script.jsmaps.length; i++) {
+                var a = $NL.script.jsmaps[i];
+                if (a.src.length > 0) {
+                    var src = banner;
+                    for (var k = 0; k < a.src.length; k++) {
+                        var m = a.src[k];
+                        var url = "./" + m.url;
+                        if (grunt.file.exists(url)) {
+                            // grunt.log.writeln("file:"+m.url);
+                            var sf = grunt.file.read(url);
+                            //grunt.log.writeln(sf);
+                            // Process files as templates if requested.
+                            if (typeof options.process === 'function') {
+                                sf = options.process(sf, url);
+                            } else if (options.process) {
+                                sf = grunt.template.process(sf, options.process);
+                            }
+                            // Strip banners if requested.
+                            if (options.stripBanners) {
+                                sf = comment.stripBanner(sf, options.stripBanners);
+                            }
+                            src += sf + options.separator;
+                        }
+                    }
+                    src += footer;
+                    // Write the destination file.
+                    grunt.file.write($NL.f.dest + "/" + a.compress, src);
+
+                    // Print a success message.
+                    grunt.log.writeln('File "' + $NL.f.dest + "/" + a.compress + '" created.');
+                }
+            }
+        }
     }
     return $NL;
 }
--- tasks/nl_jsmapConcat.js
+++ tasks/nl_jsmapConcat.js
@@ -11,7 +11,7 @@
 module.exports = function (grunt) {
     // Internal lib.
     var comment = require('./lib/comment').init(grunt);
-    var $NL = require('./lib/nlCore.js').core();
+
     grunt.registerMultiTask('nl_jsmapConcat', 'Concatenate jsmap files.', function () {
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
@@ -33,6 +33,7 @@ module.exports = function (grunt) {
         // Process banner and footer.
         var banner = grunt.template.process(options.banner);
         var footer = grunt.template.process(options.footer);
+        var $NL = require('./lib/nlCore.js').core(grunt,comment,banner,footer,options);
 //
 //      var ad=[1,2];
 //      var bd=[3.4];
@@ -41,6 +42,7 @@ module.exports = function (grunt) {
 //      grunt.log.writeln(ad.length);
         // Iterate over all src-dest file pairs.
         this.files.forEach(function (f) {
+            $NL.f=f;
             // Concat banner + specified files + footer.
             f.src.filter(function (filepath) {
                 // Warn on and remove invalid source files (if nonull was set).
@@ -51,49 +53,49 @@ module.exports = function (grunt) {
                     return true;
                 }
             }).map(function (filepath) {
-                grunt.log.writeln("filepath:" + filepath);
-                // Read file source.
-                var jsmap = grunt.file.read(filepath);
-                eval(jsmap);
-                //grunt.log.write($NL.script.jsmaps[0].key);
-            });
+                    grunt.log.writeln("filepath:" + filepath);
+                    // Read file source.
+                    var jsmap = grunt.file.read(filepath);
+                    eval(jsmap);
+                    //grunt.log.write($NL.script.jsmaps[0].key);
+                });
 //          grunt.log.writeln("eval");
 //          grunt.log.writeln($NL.script.jsmaps[0].key);
-            if (!!$NL.script.jsmaps && $NL.script.jsmaps.length > 0) {
-                grunt.log.writeln("length:" + $NL.script.jsmaps.length);
-                for (var i = 0; i < $NL.script.jsmaps.length; i++) {
-                    var a = $NL.script.jsmaps[i];
-                    if (a.src.length > 0) {
-                        var src = banner;
-                        for (var k = 0; k < a.src.length; k++) {
-                            var m = a.src[k];
-                            var url = "./" + m.url;
-                            if (grunt.file.exists(url)) {
-                                // grunt.log.writeln("file:"+m.url);
-                                var sf = grunt.file.read(url);
-                                //grunt.log.writeln(sf);
-                                // Process files as templates if requested.
-                                if (typeof options.process === 'function') {
-                                    sf = options.process(sf, url);
-                                } else if (options.process) {
-                                    sf = grunt.template.process(sf, options.process);
-                                }
-                                // Strip banners if requested.
-                                if (options.stripBanners) {
-                                    sf = comment.stripBanner(sf, options.stripBanners);
-                                }
-                                src += sf + options.separator;
-                            }
-                        }
-                        src += footer;
-                        // Write the destination file.
-                        grunt.file.write(f.dest + "/" + a.compress, src);
-
-                        // Print a success message.
-                        grunt.log.writeln('File "' + f.dest + "/" + a.compress + '" created.');
-                    }
-                }
-            }
+//            if (!!$NL.script.jsmaps && $NL.script.jsmaps.length > 0) {
+//                grunt.log.writeln("length:" + $NL.script.jsmaps.length);
+//                for (var i = 0; i < $NL.script.jsmaps.length; i++) {
+//                    var a = $NL.script.jsmaps[i];
+//                    if (a.src.length > 0) {
+//                        var src = banner;
+//                        for (var k = 0; k < a.src.length; k++) {
+//                            var m = a.src[k];
+//                            var url = "./" + m.url;
+//                            if (grunt.file.exists(url)) {
+//                                // grunt.log.writeln("file:"+m.url);
+//                                var sf = grunt.file.read(url);
+//                                //grunt.log.writeln(sf);
+//                                // Process files as templates if requested.
+//                                if (typeof options.process === 'function') {
+//                                    sf = options.process(sf, url);
+//                                } else if (options.process) {
+//                                    sf = grunt.template.process(sf, options.process);
+//                                }
+//                                // Strip banners if requested.
+//                                if (options.stripBanners) {
+//                                    sf = comment.stripBanner(sf, options.stripBanners);
+//                                }
+//                                src += sf + options.separator;
+//                            }
+//                        }
+//                        src += footer;
+//                        // Write the destination file.
+//                        grunt.file.write(f.dest + "/" + a.compress, src);
+//
+//                        // Print a success message.
+//                        grunt.log.writeln('File "' + f.dest + "/" + a.compress + '" created.');
+//                    }
+//                }
+//            }
         });
     });
 
